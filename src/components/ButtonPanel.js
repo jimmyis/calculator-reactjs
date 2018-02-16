f8ec4f88eec30dadd139c3abeaac70ed9bfff544
jimmyis <jimmy.israkhan@gmail.com>
2018-02-16 20:02:52 +0700

    ADDED buttons placement in 'ButtonPanel` component

--- src/components/ButtonPanel.js
+++ src/components/ButtonPanel.js
@@ -5,6 +5,37 @@ class ButtonPanel extends React.Component {
     return (
       <div>
         This is button panel
+        <div>
+          <div>
+            <div class="button">AC</div>
+            <div class="button">+/-</div>
+            <div class="button">%</div>
+            <div class="button">÷</div>
+          </div>
+          <div>
+            <div class="button">7</div>
+            <div class="button">8</div>
+            <div class="button">9</div>
+            <div class="button">x</div>
+          </div>
+          <div>
+            <div class="button">4</div>
+            <div class="button">5</div>
+            <div class="button">6</div>
+            <div class="button">-</div>
+          </div>
+          <div>
+            <div class="button">1</div>
+            <div class="button">2</div>
+            <div class="button">3</div>
+            <div class="button">+</div>
+          </div>
+          <div>
+            <div class="button">0</div>
+            <div class="button">.</div>
+            <div class="button">=</div>
+          </div>
+        </div>
       </div>
     );
   }
