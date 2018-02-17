1a4a700e1b7e061a23cbca669336f850c417f055
jimmyis <jimmy.israkhan@gmail.com>
2018-02-17 12:44:24 +0700

    ADDED `Calculation function` to use as main logic, expect it to return a new calculated state

--- src/components/App.js
+++ src/components/App.js
@@ -6,25 +6,38 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      total: null,
-      next: null,
+      total: null, // Keep summarization of all operation and display
+      toCalculate: null, // Keep next value to operate with 'total' prop
+      operation: null, // Keep the value that pass from each button ('buttonName' in this code)
     };
   }
 
   whenClick = (buttonName) => {
-    this.setState({total: buttonName})
+    console.log(calc(this.state, buttonName)) // Output to console, to debug
+    this.setState(calc(this.state, buttonName))
   }
 
   render() {
     return (
       <div className="component-app">
         <div id="app-name">
-          Calculator App created by Jimmyis
+          Calculator App created by Jimmyis { this.toCalculate }
         </div>
-        <ScreenPanel value={this.state.next || this.state.total || '0'}/>
+        <ScreenPanel value={this.state.toCalculate || this.state.total || '0'}/>
         <ButtonPanel clickHandler={this.whenClick}/>
       </div>
     );
   }
 }
 export default App;
+
+// Calculation function
+function calc (state, button) {
+  console.log(button)
+  
+  return {
+    total: button,
+    toCalculate: button,
+    operation: button,
+  };
+}
